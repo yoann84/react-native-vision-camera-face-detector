9fc54654d5458bb01e7e35f6562c7163c3df4a38
Yoann <yoann.le-hir@orange.fr>
2025-10-31 10:28:51 +0000

    clean project and update dep

--- example/app.config.js
+++ example/app.config.js
@@ -1,55 +1,70 @@
 export default {
   expo: {
+    newArchEnabled: true,
     name: 'Face Detector Example',
-    slug: "face-detector-example",
+    slug: 'face-detector-example',
     version: '1.0.0',
-    jsEngine: 'hermes',
-    orientation: "portrait",
-    icon: "./assets/icon.png",
+    orientation: 'portrait',
+    icon: './assets/icon.png',
     splash: {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    assetBundlePatterns: ['**/*'],
     ios: {
       bundleIdentifier: 'com.facedetector.example',
       buildNumber: '1',
       privacyManifests: {
-        NSPrivacyAccessedAPITypes: [ {
-          NSPrivacyAccessedAPIType: "NSPrivacyAccessedAPICategoryUserDefaults",
-          NSPrivacyAccessedAPITypeReasons: [ "CA92.1" ]
-        } ]
-      }
+        NSPrivacyAccessedAPITypes: [
+          {
+            NSPrivacyAccessedAPIType:
+              'NSPrivacyAccessedAPICategoryUserDefaults',
+            NSPrivacyAccessedAPITypeReasons: ['CA92.1'],
+          },
+        ],
+      },
     },
     android: {
       package: 'com.facedetector.example',
       versionCode: 1,
       adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      }
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
     },
     plugins: [
-      [ 'react-native-vision-camera', {
-        "cameraPermissionText": '$(PRODUCT_NAME) needs to access your device\'s camera.'
-      } ],
-      [ 'expo-build-properties', {
-        'android': {
-          // android 8
-          minSdkVersion: 26,
-          // android 14
-          compileSdkVersion: 34,
-          targetSdkVersion: 34,
-          buildToolsVersion: '34.0.0'
+      [
+        'react-native-vision-camera',
+        {
+          cameraPermissionText:
+            "$(PRODUCT_NAME) needs to access your device's camera.",
         },
-        ios: {
-          deploymentTarget: '14.0',
-          useFrameworks: 'static'
-        }
-      } ]
-    ]
-  }
-}
+      ],
+      [
+        'expo-image-picker',
+        {
+          photosPermission:
+            'The app accesses your photos to let you share them with your friends.',
+        },
+      ],
+      [
+        'expo-build-properties',
+        {
+          android: {
+            // android 8
+            minSdkVersion: 26,
+            // android 14
+            compileSdkVersion: 35,
+            targetSdkVersion: 35,
+            buildToolsVersion: '35.0.0',
+          },
+          ios: {
+            deploymentTarget: '15.5',
+            useFrameworks: 'static',
+          },
+        },
+      ],
+    ],
+  },
+};
