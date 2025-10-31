98fa68dace4885976fff0ffae61263f8cbfee7d5
Yoann <yoann.le-hir@orange.fr>
2025-10-31 12:05:29 +0000

    config

---
+++ react-native.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      /**
+       * @type {import('@react-native-community/cli-types').IOSDependencyParams}
+       */
+      ios: {
+        podspecPath: './VisionCameraFaceDetector.podspec',
+      },
+      /**
+       * @type {import('@react-native-community/cli-types').AndroidDependencyParams}
+       */
+      android: {
+        sourceDir: './android',
+        packageImportPath: 'import com.visioncamerafacedetector.VisionCameraFaceDetectorPluginPackage;',
+      },
+    },
+  },
+}
+
